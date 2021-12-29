ae97b75b861277edc9b2df3f3ef421f7c3af096d
Vincent <vincentbusiness55@gmail.com>
2021-12-28 17:41:02 -0800

    Initial commit

---
+++ archlinux.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+echo "hey btw this only installs packages cause i am too lazy to do that, you still have to setup everything else."
+git clone "https://aur.archlinux.org/yay.git"
+cd ${HOME}/yay
+makepkg -si --noconfirm
+cd ~
+PKGS=(
+'plasma-desktop'
+'nvidia'
+'pipewire'
+)
+AURPKGS=(
+'spotify'
+'steam'
+'microsoft-edge-stable-bin'
+)
+echo "ensuring pip"
+python3 -m ensure-pip
+useradd -m Vincent Jose
+echo "installing the big boys"
+for PKG in "${PKGS[@]}"; do
+  echo "Now installing ${PKG}"
+  sudo pacman -S "$PKG" --noconfirm --needed
+for PKG in "${AURPKGS[@]}"; do
+  echo "Now installing ${PKG}"
+  sudo yay -S --noconfirm $PKG
